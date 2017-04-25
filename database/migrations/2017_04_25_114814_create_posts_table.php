d78d871ab51c2c3f1f1069315eeeb6e6cb27f690
TPL <yumash@ukr.net>
2017-04-25 15:17:17 +0300

    add models, migrations, seeds and auth

---
+++ database/migrations/2017_04_25_114814_create_posts_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title');
+            $table->text('body');
+            $table->string('slug')->unique();
+            $table->string('image');
+            $table->boolean('published')->default(false);
+            $table->unsignedInteger('user_id');
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
---
+++ database/seeds/RolesSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Role;
+
+class RolesSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $author = Role::create([
+            'name' => 'Author',
+            'slug' => 'author',
+            'permissions' => [
+                'create-post' => true,
+                'update-post' => true,
+                'publish-post' => true,
+            ]
+        ]);
+        $reader = Role::create([
+            'name' => 'Editor',
+            'slug' => 'editor',
+            'permissions' => [
+                'comment-post' => true,
+            ]
+        ]);
+    }
+}
